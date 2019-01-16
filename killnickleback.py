7d23dbaf39e8d768c0ddfad553208e688010889a
Daniel Combs <danielcombs@Daniels-MacBook-Pro.local>
2019-01-16 10:05:46 -0600

    completed

---
+++ killnickleback.py
@@ -0,0 +1,16 @@
+
+# Define a set that contains tuples. Each tuple should contain two strings:
+t
+songs = {
+    ('Nickelback', 'How You Remind Me'),
+    ('Will.i.am', 'That Power'),
+    ('Miles Davis', 'Stella by Starlight'),
+    ('Nickelback', 'Animals')
+}
+
+
+#Using a set comprehension, create a new set that contains all songs that were not performed by Nickelback.
+new_list = [f' {k}, {i} ' for k,i in songs if k != 'Nickelback']
+
+print(songs)
+print(new_list)
